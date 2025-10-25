3fd7692480a3da3b3d66493f32727cd07654c297
Divyansh <divyanshtyagi0036@gmail.com>
2025-10-26 00:47:35 +0530

    dialog box added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+
+
+import InputBox from './components/InputBox'
+
 import AboutPage from './pages/AboutPage' // Assuming this path is correct
 import ContactPage from './pages/ContactPage'
 
 function App() {
   return (
+    <>
     <BrowserRouter>
       {/* 1. Changed to a subtle gradient background */}
       <div className="min-h-screen bg-gradient-to-b from-blue-50 to-blue-100">
@@ -13,7 +18,7 @@ function App() {
         {/* 2. Set the global top padding here to clear the fixed header */}
         <main className="">
           <Routes>
-            <Route path="/" element={<div className="p-8">Home Page</div>} />
+            <Route path="/" element={ <InputBox/>} />
             {/* <Route path="/trips" element={<div className="p-8">Trips Page</div>} /> */}
             
             <Route path="/about" element={<AboutPage />} />
@@ -24,6 +29,9 @@ function App() {
         
       </div>
     </BrowserRouter>
+   
+    </>
+    
   )
 }
 
